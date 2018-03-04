2c0f7cf5a1469a56c9b721174977233cec76b580
Brian Kurek <brkurek@gmail.com>
2018-03-04 13:04:38 -0600

    Service tests (except login)

--- src/app/service/cognito.service.spec.ts
+++ src/app/service/cognito.service.spec.ts
@@ -3,13 +3,23 @@ import { TestBed, inject } from '@angular/core/testing';
 import { CognitoService } from './cognito.service';
 
 describe('CognitoService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CognitoService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [CognitoService]
+        });
     });
-  });
 
-  it('should be created', inject([CognitoService], (service: CognitoService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should be created', inject([CognitoService], (service: CognitoService) => {
+        expect(service).toBeTruthy();
+    }));
+
+    it('should return a valid user pool', inject([CognitoService], (service: CognitoService) => {
+        const userPool = service.getUserPool();
+        expect(userPool).toBeTruthy();
+    }));
+
+    it('should return a defined current user', inject([CognitoService], (service: CognitoService) => {
+        const user = service.getCurrentUser();
+        expect(user).toBeDefined();
+    }));
 });
